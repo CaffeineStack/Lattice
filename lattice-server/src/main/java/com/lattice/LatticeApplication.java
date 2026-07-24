5cce92f4ba2cf7535e07c5e019c9d8bf3716b228
Samhitha <samhitha262706@gmail.com>
2026-07-24 23:24:28 +0530

    Initial project setup

---
+++ lattice-server/src/main/java/com/lattice/LatticeApplication.java
@@ -0,0 +1,12 @@
+package com.lattice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatticeApplication.class, args);
+    }
+}
